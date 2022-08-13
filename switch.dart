4471f85ba4413ce024def849de4a4660f8399757
David Felix <davidffelix@hotmail.com>
2022-08-12 21:22:00 -0300

    Switch

---
+++ switch.dart
@@ -0,0 +1,17 @@
+void main() {
+  int ValorInt = 5;
+  switch(ValorInt) {
+    case 0:
+      print('Zero');
+      break;
+    case 1:
+      print('Um');
+      break;
+    case 2:
+      print('Dois');
+      break;
+    default:
+      print('Padrao');
+
+  }
+}
