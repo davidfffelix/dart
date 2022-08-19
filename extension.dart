7a3afb685b9b883043dc19c25d9c5b532efd38bb
David Felix <davidffelix@hotmail.com>
2022-08-19 18:41:27 -0300

    Extension

--- extension.dart
+++ extension.dart
@@ -1,25 +1,18 @@
-imprimirProduto(int qtde, {String? nome, double? preco}) {
-  for (var i = 0; i < qtde; i++) {
-    print('O produto ${nome} tem preço ${preco}.');
+void main() {
+  String nome = 'joao';
+  print(EnumTest.enumValue.toValue());
+  print(EnumTest.enumNovo.toValue());
+}
+enum EnumTest {
+  enumValue, enumNovo;
+}
+
+extension ExtensionsEnum on Enum {
+  String toValue() {
+    Map map = {
+      EnumTest.enumValue: 'xpto',
+      EnumTest.enumNovo: 'novoValor'
+    };
+    return map[this];
   }
-}
-
-class Produto {
-  String? nome;
-  double? preco;
-
-  Produto({this.nome, this.preco = 0.99});
-}
-
-main() {
-  var p1 = Produto(nome: 'Lapis');
-  var p2 = Produto(nome: 'Notebook', preco: 4000);
-  //p1.nome = 'Lapis';
-  //p1.preco = 4.65;
-
-  //print('O produto ${p1.nome} tem preço ${p1.preco}.');
-  //print('O produto ${p2.nome} tem preço ${p2.preco}.');
-
-  imprimirProduto(1, nome: p1.nome!, preco: p1.preco!);
-  imprimirProduto(20, preco: p2.preco!, nome: p2.nome!);
 }
